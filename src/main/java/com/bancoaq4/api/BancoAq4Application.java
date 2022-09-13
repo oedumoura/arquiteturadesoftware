c7543ecd8c3de950abf316ea9bc32ea9b4f8003c
oedumoura <edurdmoura@gmail.com>
2022-09-12 21:35:14 -0300

    Inicio

---
+++ src/main/java/com/bancoaq4/api/BancoAq4Application.java
@@ -0,0 +1,13 @@
+package com.bancoaq4.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoAq4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancoAq4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bancoaq4/api/BancoAq4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoaq4.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoAq4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
